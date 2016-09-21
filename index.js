3de15115d1ef915f76a2023a8ed30ad917d78e39
robdel12 <robertdeluca19@gmail.com>
2016-09-21 09:19:06 -0500

    Very beginnings of the styles addon.

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'fs-branding'
+  name: 'fs-branding',
+  // TODO: Remove when done
+  isDevelopingAddon: function() { return true; },
+
+  included: function(app) {
+    this._super.included(app);
+  }
 };
