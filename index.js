735b579cb973cfa1bff43d905888c08c3e556e4e
robdel12 <robertdeluca19@gmail.com>
2016-09-27 15:29:56 -0500

    Add assets

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ module.exports = {
   isDevelopingAddon: function() { return true; },
 
   included: function(app) {
+    process.env.enableAssets = app.options.fsBranding.enableAssets || false;
+
     this._super.included(app);
+  },
+
+  treeForPublic: function(tree) {
+    if(process.env.enableAssets !== 'false') {
+      return tree;
+    }
   }
 };
